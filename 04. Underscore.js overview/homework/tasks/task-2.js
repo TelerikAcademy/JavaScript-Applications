f4654dfd8396e2cf4d54b7b2bd09dfbac5308b66
Doncho Minkov <DonchoMinkov@gmail.com>
2015-08-19 14:36:54 +0300

    Added task descriptions

--- 04. Underscore.js overview/homework/tasks/task-2.js
+++ 04. Underscore.js overview/homework/tasks/task-2.js
@@ -2,7 +2,9 @@
 Create a function that:
 *   Takes an array of students
     *   Each student has a `firstName`, `lastName` and `age` properties
-*   **finds** the first name and last name of all students with age between 18 and 24. Use Underscore.js
+*   **finds** the students whose age is between 18 and 24
+*   **prints**  the fullname of found students
+    *   fullname is the concatenation of `firstName`, ' ' (empty space) and `lastName`
 */
 
 function solve(){
---
+++ 04. Underscore.js overview/homework/tasks/task-3.js
@@ -0,0 +1,18 @@
+/* 
+Create a function that:
+*   Takes an array of students
+    *   Each student has:
+        *   `firstName`, `lastName` and `age` properties
+        *   Array of marks 
+            *   Each mark has properties `subject` and `value`        
+*   **finds** the student with highest average mark
+*   **prints** to the console  'FOUND_STUDENT_FULLNAME has an average score of MARK_OF_THE_STUDENT'
+    *   fullname is the concatenation of `firstName`, ' ' (empty space) and `lastName`
+*/
+
+function solve(){
+  return function (students) {
+  };
+}
+
+module.exports = solve;
