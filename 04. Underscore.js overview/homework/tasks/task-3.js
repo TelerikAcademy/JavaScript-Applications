d1cf9d3c06a795fda5952c89b644f6d10b4e77e1
ginovski <sashko.ginovski@gmail.com>
2015-08-21 23:27:22 +0300

    Edited Underscore.js homework task-3 description

--- 04. Underscore.js overview/homework/tasks/task-3.js
+++ 04. Underscore.js overview/homework/tasks/task-3.js
@@ -3,8 +3,7 @@ Create a function that:
 *   Takes an array of students
     *   Each student has:
         *   `firstName`, `lastName` and `age` properties
-        *   Array of marks 
-            *   Each mark has properties `subject` and `value`        
+        *   Array of decimal numbers representing the marks         
 *   **finds** the student with highest average mark (there will be only one)
 *   **prints** to the console  'FOUND_STUDENT_FULLNAME has an average score of MARK_OF_THE_STUDENT'
     *   fullname is the concatenation of `firstName`, ' ' (empty space) and `lastName`
