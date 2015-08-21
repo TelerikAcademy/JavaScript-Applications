90b51c18a6c9dedabb7c18dc31d5942098d59c58
Kristiyan Tsaklev <ktsaklev@gmail.com>
2015-08-21 17:49:53 +0300

    Description fixes

--- 04. Underscore.js overview/homework/tasks/task-4.js
+++ 04. Underscore.js overview/homework/tasks/task-4.js
@@ -5,17 +5,18 @@ Create a function that:
 *   **groups** the animals by `species`
     *   the groups are sorted by `species` descending
 *   **sorts** them ascending by `legsCount`
+	*	if two animals have the same number of legs sort them by name
 *   **prints** them to the console in the format:
 
 ```
-    ----------- (number of underscores is equal to the length of the GROUP_1_NAME + 1)
+    ----------- (number of dashes is equal to the length of the GROUP_1_NAME + 1)
     GROUP_1_NAME:
-    ----------- (number of underscores is equal to the length of the GROUP_1_NAME + 1)
+    ----------- (number of dashes is equal to the length of the GROUP_1_NAME + 1)
     NAME has LEGS_COUNT legs //for the first animal in group 1
     NAME has LEGS_COUNT legs //for the second animal in group 1
-    ----------- (number of underscores is equal to the length of the GROUP_2_NAME + 1)
+    ----------- (number of dashes is equal to the length of the GROUP_2_NAME + 1)
     GROUP_2_NAME:
-    ----------- (number of underscores is equal to the length of the GROUP_2_NAME + 1)
+    ----------- (number of dashes is equal to the length of the GROUP_2_NAME + 1)
     NAME has LEGS_COUNT legs //for the first animal in the group 2
     NAME has LEGS_COUNT legs //for the second animal in the group 2
     NAME has LEGS_COUNT legs //for the third animal in the group 2
