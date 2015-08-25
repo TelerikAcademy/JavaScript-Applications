e910724343790cb9a58f9f53f8d6b0167f64aee5
EvlogiHr <evlogihristov@yahoo.com>
2015-08-25 12:41:51 +0300

    Added demo for simple promise creation

--- 06. Promises and asynchronous programming/demos/2. promises/2.1. promise-creation.js
+++ 06. Promises and asynchronous programming/demos/2. promises/2.1. promise-creation.js
@@ -1,3 +1,16 @@
 (function() {
+    var promise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve({id: 5, decription: 'some data'})
+            // reject('somethisng very bad happened')
+        }, 2000);
+    });
 
+    promise
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(function(eror) {
+            console.log(eror);
+        });
 }());
