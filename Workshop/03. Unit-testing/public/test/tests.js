2eedecbd5eb9afb929c0e965c0e83b0cf5e62dc9
Kristiyan Tsaklev <cuki@cukii.tk>
2016-09-30 19:59:29 +0300

    Add register tests

--- Workshop/03. Unit-testing/public/test/tests.js
+++ Workshop/03. Unit-testing/public/test/tests.js
@@ -4,10 +4,60 @@ const {expect, assert} = chai;
 
 describe('Tests', function() {
 	describe('Get cookies tests', function() {
-		it('expect cookies() to return a response', function(done) {
+		const result = {
+			result: []
+		};
+
+		beforeEach(function() {
+			sinon.stub(requester, 'getJSON', (route) => {
+				return new Promise((resolve, reject) => {
+					if(route !== '/api/cookies') {
+						reject(new Error(`Invalid route ${route}`));
+						return;
+					}
+					resolve(result);
+				});
+			});
+		});
+		afterEach(function() {
+			requester.getJSON.restore();
+		});
+
+		it('expect dataService.cookies() to return correct result', function(done) {
 			dataService.cookies()
 				.then(obj => {
-					expect(obj).to.exist;
+					expect(obj).to.eql(result)
+				})
+				.then(done, done);
+		});
+	});
+
+	describe('Register tests', function() {
+
+		beforeEach(function() {
+			sinon.stub(requester, 'postJSON', (route, user) => {
+				return new Promise((resolve, reject) => {
+					if(route !== '/api/users') {
+						reject(new Error(`Invalid route ${route}`));
+						return;
+					}
+					resolve(user);
+				});
+			});
+		});
+		afterEach(function() {
+			requester.postJSON.restore();
+		});
+
+		const user = {
+			username: 'Cuki',
+			passHash: 'pesho42'
+		};
+
+		it('Register user test', function(done) {
+			dataService.register(user)
+				.then(actual => {
+					expect(actual).to.eql(user);
 				})
 				.then(done, done);
 		});
