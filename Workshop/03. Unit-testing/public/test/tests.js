40036254d3bb38af54328674e997cd35f4e69429
Kristiyan Tsaklev <cuki@cukii.tk>
2016-09-30 22:23:05 +0300

    Add the rest of the tests

--- Workshop/03. Unit-testing/public/test/tests.js
+++ Workshop/03. Unit-testing/public/test/tests.js
@@ -7,6 +7,13 @@ const user = {
 	username: 'SOME_USERNAME',
 	passHash: 'SOME_PASSHASH'
 };
+const cookie = {
+	img: 'SOME COOKIE IMG',
+	text: 'SOME COOKIE TEXT',
+	likes: 0,
+	dislikes: 0,
+	id: 42
+};
 
 describe('Tests', function() {
 	describe('Get cookies tests', function() {
@@ -221,6 +228,250 @@ describe('Tests', function() {
 				})
 				.then(f => {
 					expect(f).to.be.true;
+				})
+				.then(done, done);
+		});
+	});
+
+	describe('Logout tests', function() {
+		beforeEach(function() {
+			sinon.stub(requester, 'putJSON')
+				.returns(new Promise((resolve, reject) => {
+					resolve({
+						result: {
+							username: user.username,
+							authKey: AUTH_KEY
+						}
+					});
+				}));
+			localStorage.clear();
+		});
+		afterEach(function() {
+			requester.putJSON.restore();
+			localStorage.clear();
+		});
+
+		it('expect localStorage to have no username after logout', function(done) {
+			dataService.login()
+				.then(() => {
+					return dataService.logout();
+				})
+				.then(() => {
+					expect(localStorage.getItem('username')).to.be.null;
+				})
+				.then(done, done);
+		});
+
+		it('expect localStorage to have no authKey after logout', function(done) {
+			dataService.login()
+				.then(() => {
+					return dataService.logout();
+				})
+				.then(() => {
+					expect(localStorage.getItem('authKey')).to.be.null;
+				})
+				.then(done, done);
+		});
+	});
+
+	describe('Add cookie tests', function() {
+		let cookiesDB = [];
+
+		beforeEach(function() {
+			sinon.stub(requester, 'postJSON', (route, cookie, options) => {
+				return new Promise((resolve, reject) => {
+					cookiesDB.push(cookie);
+					// add userId to cookie?
+					resolve(cookie);
+				});
+			});
+			sinon.stub(requester, 'putJSON')
+				.returns(new Promise((resolve, reject) => {
+					resolve({
+						result: {
+							username: user.username,
+							authKey: AUTH_KEY
+						}
+					});
+				}));
+			localStorage.clear();
+			cookiesDB = [];
+		});
+		afterEach(function() {
+			requester.postJSON.restore();
+			requester.putJSON.restore();
+			localStorage.clear();
+		});
+
+		it('expect postJSON to be called for cookie', function(done) {
+			dataService.login(user)
+				.then(() => {
+					return dataService.addCookie(cookie);
+				})
+				.then(() => {
+					expect(requester.postJSON.calledOnce).to.be.true;
+				})
+				.then(done, done);
+		});
+		it('expect postJSON to be called with correct route', function(done) {
+			dataService.login(user)
+				.then(() => {
+					return dataService.addCookie(cookie);
+				})
+				.then(() => {
+					const actual = requester.postJSON
+						.firstCall
+						.args[0];
+					expect(actual).to.equal('/api/cookies');
+				})
+				.then(done, done);
+		});
+		it('expect postJSON to be called with the cookie', function(done) {
+			dataService.login(user)
+				.then(() => {
+					return dataService.addCookie(cookie);
+				})
+				.then(() => {
+					const actual = requester.postJSON
+						.firstCall
+						.args[1];
+					expect(actual).to.eql(cookie);
+				})
+				.then(done, done);
+		});
+		it('expect postJSON to be called with authorization headers', function(done) {
+			dataService.login(user)
+				.then(() => {
+					return dataService.addCookie(cookie);
+				})
+				.then(() => {
+					const actual = requester.postJSON
+						.firstCall
+						.args[2];
+					expect(actual.headers[HTTP_HEADER_KEY]).to.equal(AUTH_KEY);
+				})
+				.then(done, done);
+		});
+		it('expect cookie to be added to cookieDB', function(done) {
+			dataService.login(user)
+				.then(() => {
+					return dataService.addCookie(cookie);
+				})
+				.then(() => {
+					expect(cookiesDB).to.eql([cookie]);
+				})
+				.then(done, done);
+		});
+	});
+
+	describe('Rate cookie tests', function() {
+		let cookiesDB = [];
+
+		beforeEach(function() {
+			sinon.stub(requester, 'putJSON', (route, type, options) => {
+				return new Promise((resolve, reject) => {
+					if(route === '/api/auth') {
+						resolve({
+							result: {
+								username: user.username,
+								authKey: AUTH_KEY
+							}
+						});
+						return;
+					};
+
+					const cookieId = +route.split('/')[3];
+					const cookieToRate = cookiesDB.find((c) => c.id === cookieId);
+
+					if(type.type.type === 'like') {
+						cookieToRate.likes += 1;
+					}
+					else if(type.type.type === 'dislike') {
+						cookieToRate.dislikes += 1;
+					}
+
+					resolve(cookieToRate);
+				});
+			});
+			localStorage.clear();
+			cookiesDB = [cookie];
+		});
+		afterEach(function() {
+			requester.putJSON.restore();
+			localStorage.clear();
+		});
+
+		it('expect putJSON to be called for rating', function(done) {
+			dataService.login(user)
+				.then(() => {
+					return dataService.rateCookie(cookie.id, {type:'like'});
+				})
+				.then(() => {
+					expect(requester.putJSON.calledTwice).to.be.true;
+				})
+				.then(done, done);
+		});
+		it('expect cookie rating to be done with correct route', function(done) {
+			dataService.login(user)
+				.then(() => {
+					return dataService.rateCookie(cookie.id, {type:'like'});
+				})
+				.then(() => {
+					const actual = requester.putJSON
+						.secondCall
+						.args[0];
+					expect(actual).to.equal(`/api/cookies/${cookie.id}`);
+				})
+				.then(done, done);
+		});
+		it('expect cookie rating type to be the used type of rating', function(done) {
+			dataService.login(user)
+				.then(() => {
+					return dataService.rateCookie(cookie.id, {type:'like'});
+				})
+				.then(() => {
+					const actual = requester.putJSON
+						.secondCall
+						.args[1];
+					expect(actual.type.type).to.equal('like');
+				})
+				.then(done, done);
+		});
+		it('expect cookie rating to be called for the given cookie id', function(done) {
+			dataService.login(user)
+				.then(() => {
+					return dataService.rateCookie(cookie.id, {type:'like'});
+				})
+				.then(() => {
+					const actual = requester.putJSON
+						.secondCall
+						.args[2];
+					expect(actual.headers[HTTP_HEADER_KEY]).to.equal(AUTH_KEY);
+				})
+				.then(done, done);
+		});
+		
+		it('expect liking a cookie to increment likes value of cookie', function(done) {
+			let previousLikes = cookiesDB[0].likes;
+
+			dataService.login(user)
+				.then(() => {
+					return dataService.rateCookie(cookie.id, {type:'like'});
+				})
+				.then(() => {
+					expect(cookiesDB[0].likes).to.equal(previousLikes + 1);
+				})
+				.then(done, done);
+		});
+		it('expect disliking a cookie to increment dislikes value of cookie', function(done) {
+			let previousDislikes = cookiesDB[0].dislikes;
+
+			dataService.login(user)
+				.then(() => {
+					return dataService.rateCookie(cookie.id, {type:'dislike'});
+				})
+				.then(() => {
+					expect(cookiesDB[0].dislikes).to.equal(previousDislikes + 1);
 				})
 				.then(done, done);
 		});
