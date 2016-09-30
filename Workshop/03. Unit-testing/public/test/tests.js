3f3ebdaaeae92079a7949f3547982fe4ce26534f
Kristiyan Tsaklev <cuki@cukii.tk>
2016-09-30 21:23:39 +0300

    More unit tests

--- Workshop/03. Unit-testing/public/test/tests.js
+++ Workshop/03. Unit-testing/public/test/tests.js
@@ -2,19 +2,6 @@ mocha.setup('bdd');
 
 const {expect, assert} = chai;
 
-function checkRoute(actual, expected, reject) {
-	if(actual !== expected) {
-		reject(new Error(`Invalid route ${actual}`));
-	}
-}
-
-function checkUserData(user, reject) {
-	const prop = Object.keys(user).sort();
-	if(prop.length !== 2 || prop[0] !== 'passHash' || prop[1] !== 'username') {
-		reject(new Error(`Invalid user data`));
-	}
-}
-
 const AUTH_KEY = "SOME_AUTH_KEY";
 const user = {
 	username: 'SOME_USERNAME',
@@ -28,17 +15,26 @@ describe('Tests', function() {
 		};
 
 		beforeEach(function() {
-			sinon.stub(requester, 'getJSON', (route) => {
-				return new Promise((resolve, reject) => {
-					checkRoute(route, '/api/cookies', reject);
+			sinon.stub(requester, 'getJSON')
+				.returns(new Promise((resolve, reject) => {
 					resolve(result);
-				});
-			});
+				}));
 		});
 		afterEach(function() {
 			requester.getJSON.restore();
 		});
 
+		it('expect dataService.cookies() to make correct getJSON call', function(done) {
+			dataService.cookies()
+				.then(obj => {
+					const actual = requester.getJSON
+						.firstCall
+						.args[0];
+
+					expect(actual).to.equal('/api/cookies');
+				})
+				.then(done, done);
+		});
 		it('expect dataService.cookies() to return correct result', function(done) {
 			dataService.cookies()
 				.then(obj => {
@@ -50,51 +46,82 @@ describe('Tests', function() {
 
 	describe('Register tests', function() {
 		beforeEach(function() {
-			sinon.stub(requester, 'postJSON', (route, user) => {
-				return new Promise((resolve, reject) => {
-					checkRoute(route, '/api/users', reject);
-					checkUserData(user, reject);
+			sinon.stub(requester, 'postJSON')
+				.returns(new Promise((resolve, reject) => {
 					resolve(user);
-				});
-			});
+				}));
 		});
 		afterEach(function() {
 			requester.postJSON.restore();
 		});
 
-		it('Expect registering of user to return the user', function(done) {
+		it('expect dataService.register() to make correct postJSON call', function(done) {
+			dataService.register(user)
+				.then(() => {
+					const actual = requester.postJSON
+						.firstCall
+						.args[0];
+					expect(actual).to.equal('/api/users');
+				})
+				.then(done, done);
+		});
+		it('expect dataService.register() to post correct user data', function(done) {
+			dataService.register(user)
+				.then(() => {
+					const actual = requester.postJSON
+						.firstCall
+						.args[1];
+					const prop = Object.keys(actual).sort();
+					expect(prop.length).to.equal(2);
+					expect(prop[0]).to.equal('passHash');
+					expect(prop[1]).to.equal('username');
+				})
+				.then(done, done);
+		});
+		it('expect dataService.register() to make correct postJSON call', function(done) {
+			dataService.register(user)
+				.then(() => {
+					const actual = requester.postJSON
+						.firstCall
+						.args[0];
+					expect(actual).to.equal('/api/users');
+				})
+				.then(done, done);
+		});
+		it('expect dataService.register() to post correct user data', function(done) {
+			dataService.register(user)
+				.then(() => {
+					const actual = requester.postJSON
+						.firstCall
+						.args[1];
+					const prop = Object.keys(actual).sort();
+					expect(prop.length).to.equal(2);
+					expect(prop[0]).to.equal('passHash');
+					expect(prop[1]).to.equal('username');
+				})
+				.then(done, done);
+		});
+
+		it('expect registering of user to return the user', function(done) {
 			dataService.register(user)
 				.then(actual => {
 					expect(actual).to.eql(user);
 				})
 				.then(done, done);
 		});
-		it('Expect registering invalid data to fail', function(done) {
-			dataService.register(42)
-				.then(actual => {
-					done(new Error('User data is valid'));
-				},
-				(err) => {
-					done();
-				});
-		});
 	});
 
 	describe('Login tests', function() {
 		beforeEach(function() {
-			sinon.stub(requester, 'putJSON', (route, user) => {
-				return new Promise((resolve, reject) => {
-					checkRoute(route, '/api/auth', reject);
-					checkUserData(user, reject);
-					
+			sinon.stub(requester, 'putJSON')
+				.returns(new Promise((resolve, reject) => {
 					resolve({
 						result: {
 							username: user.username,
 							authKey: AUTH_KEY
 						}
 					});
-				});
-			});
+				}));
 			localStorage.clear();
 		});
 		afterEach(function() {
@@ -102,6 +129,29 @@ describe('Tests', function() {
 			localStorage.clear();
 		});
 
+		it('expect dataService.login() to make correct putJSON call', function(done) {
+			dataService.login(user)
+				.then(() => {
+					const actual = requester.putJSON
+						.firstCall
+						.args[0];
+					expect(actual).to.equal('/api/auth');
+				})
+				.then(done, done);
+		});
+		it('expect dataService.login() to put correct user data', function(done) {
+			dataService.login(user)
+				.then(() => {
+					const actual = requester.putJSON
+						.firstCall
+						.args[1];
+					const prop = Object.keys(actual).sort();
+					expect(prop.length).to.equal(2);
+					expect(prop[0]).to.equal('passHash');
+					expect(prop[1]).to.equal('username');
+				})
+				.then(done, done);
+		});
 		it('Expect login to login the right user and set him in localStorage', function(done) {
 			dataService.login(user)
 				.then(() => {
@@ -120,19 +170,15 @@ describe('Tests', function() {
 
 	describe('Is loggedIn tests', function() {
 		beforeEach(function() {
-			sinon.stub(requester, 'putJSON', (route, user) => {
-				return new Promise((resolve, reject) => {
-					checkRoute(route, '/api/auth', reject);
-					checkUserData(user, reject);
-					
+			sinon.stub(requester, 'putJSON')
+				.returns(new Promise((resolve, reject) => {
 					resolve({
 						result: {
 							username: user.username,
 							authKey: AUTH_KEY
 						}
 					});
-				});
-			});
+				}));
 			localStorage.clear();
 		});
 		afterEach(function() {
@@ -149,7 +195,9 @@ describe('Tests', function() {
 		});
 		it('expect to be logged in when we have logged in', function(done) {
 			dataService.login(user)
-				.then(() => dataService.isLoggedIn())
+				.then(() => {
+					return dataService.isLoggedIn();
+				})
 				.then(f => {
 					expect(f).to.be.true;
 				})
