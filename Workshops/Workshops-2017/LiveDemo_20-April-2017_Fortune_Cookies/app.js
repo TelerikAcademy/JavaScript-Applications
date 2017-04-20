ccfb48476ff1d0196601f45f18998fad64e013d9
Kristiyan Tsaklev <cuki@cukii.tk>
2017-04-20 17:00:02 +0300

    Serve node_modules/ on /libs

--- Workshops/Workshops-2017/LiveDemo_20-April-2017_Fortune_Cookies/app.js
+++ Workshops/Workshops-2017/LiveDemo_20-April-2017_Fortune_Cookies/app.js
@@ -9,6 +9,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
+app.use('/libs', express.static('node_modules'));
 
 require('./utils/authorize-user')(app, db);
 
