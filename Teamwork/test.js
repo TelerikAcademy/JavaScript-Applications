6d18bb25ccf2fa00e2dbfc627dad9dcc57d62d96
Georgi Grozdanov <grozdanovgg@gmail.com>
2017-04-22 03:01:41 +0300

    partially working

--- Teamwork/test.js
+++ Teamwork/test.js
@@ -1,21 +1,24 @@
 /*jshint esversion: 6 */
-
-let $root = $('.root'),
+let pastTime = '2',
+    pastTimeType = 'm',
+    $root = $('.root'),
     $listColZero = $root.append($('<ul/>').addClass('list')),
-    valuesArray = [],
-    arr = $
+    valuesArray = [];
+let arr = $
     .getJSON(
-        "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D'https%3A%2F%2Fchartapi.finance.yahoo.com%2Finstrument%2F1.0%2Faapl%2Fchartdata%3Btype%3Dquote%3Brange%3D2m%2Fcsv'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=",
-        calculate)
-    .error(errorHandler);
-
-
+        `https://query.yahooapis.com/v1/public/yql?q=select%20*%20` +
+        `from%20csv%20where%20url%3D'https%3A%2F%2Fchartapi.finance.yahoo.com` +
+        `%2Finstrument%2F1.0%2Faapl%2Fchartdata%3Btype%3Dquote%3Brange%3D` +
+        `${pastTime}${pastTimeType}` +
+        `%2Fcsv'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=`,
+        calculate);
 
 function calculate(data, textStatus, jqXHR) {
     data = data.query.results.row;
     data.forEach(el => {
         //check if value is only numbers => so it is the correct row
         if (/^\d+$/.test(el.col0)) {
+
             let obj = {
                 'close': el.col1,
                 'high': el.col2,
@@ -31,6 +34,5 @@ function calculate(data, textStatus, jqXHR) {
     // console.log(valuesArray.length);
 }
 
-function errorHandler() {}
 
 console.log(valuesArray);
--- Teamwork/test2.js
+++
@@ -1,4 +0,0 @@
-/*jshint esversion: 6 */
-var httpRequest = new XMLHttpRequest();
-httpRequest.open('GET', 'https://abv.bg', true);
-httpRequest.send(null);
